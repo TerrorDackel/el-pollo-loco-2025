70e40935fca8e0741317c55c4044a7d4453185f6
TerrorDackel <jenny.codes.007@gmail.com>
2025-01-21 08:17:09 +0100

    first commit

---
+++ js/chicken.class.js
@@ -0,0 +1,17 @@
+class Chicken extends MovableObject {
+
+      constructor(){
+            super().loadImage(".imgs/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+            
+      }
+
+      // moveRight() {
+
+      // }
+
+      // jump() {
+
+      // }
+
+
+}
